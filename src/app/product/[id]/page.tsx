9717359581c9427f161807b920b6f8b601bf3c77
valdenicio22 <valdenicio22@gmail.com>
2023-09-21 19:27:10 +0200

    feat: stripe integration with product page

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,27 +1,50 @@
 import { Button } from "@/components/Button";
+import { stripe } from "@/lib/stripe";
+import { formatPrice } from "@/utils/utils";
 import Image from "next/image";
+import Stripe from "stripe";
 
-export default function Product() {
+export const getProductData = async (id: string) => {
+  const response = stripe.products.retrieve(id, {
+    expand: ["default_price"],
+  });
+  return response;
+};
+
+type ProductProps = {
+  params: {
+    id: string;
+  };
+};
+export default async function Product({ params: { id } }: ProductProps) {
+  const response = await getProductData(id);
+  const { id: prodId, name, description, default_price, images } = response;
+  const productPrice = default_price as Stripe.Price;
+
+  const product = {
+    id: prodId,
+    name,
+    description,
+    price: productPrice.unit_amount ? productPrice.unit_amount / 100 : 0,
+    imageUrl: images[0],
+  };
+  console.log("response product page", response.default_price);
   return (
     <section className="container grid grid-cols-product gap-16">
       <div className="h-[41rem] flex items-center justify-center bg-gradient-bg rounded-lg">
         <Image
-          src="/assets/maratona2.svg"
+          src={product.imageUrl}
           width={520}
           height={480}
-          alt="maratona"
+          alt={product.name}
           className="object-cover bg-no-repeat"
         />
       </div>
       <div className="w-full h-full flex flex-col justify-center items-start">
-        <h2 className="text-2xl font-bold">Camiseta Maratona</h2>
-        <strong className="text-2xl text-main-light mt-4">R$ 79,90</strong>
+        <h2 className="text-2xl font-bold">{product.name}</h2>
+        <strong className="text-2xl text-main-light mt-4">{formatPrice(product.price)}</strong>
         <p className="mt-12 text-lg">
-          Tempus fermentum eget lacus, quis ante. Potenti sit pharetra,
-          ridiculus amet. Bibendum pretium arcu arcu eget viverra at metus donec
-          hendrerit. Rhoncus, nunc, eu at ac. At massa, fermentum amet ornare
-          cras tincidunt nunc tincidunt. Netus lorem nulla nulla mattis integer
-          velit dictum proin nibh.
+          {product.description}
         </p>
         <Button className="mt-auto">Colocar na sacola</Button>
       </div>
--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -1,6 +1,7 @@
 import { Product } from "@/types/product";
 import { formatPrice } from "@/utils/utils";
 import Image from "next/image";
+import Link from "next/link";
 import { CartButton } from "../CartButton";
 
 type ProductCardProps = {
@@ -10,28 +11,30 @@ type ProductCardProps = {
 
 export const ProductCard = ({ className, product }: ProductCardProps) => {
   return (
-    <div
-      className={`w-[696px] h-[656px] rounded-lg bg-gradient-bg shadow-card-shadow flex items-center justify-center cursor-pointer overflow-hidden relative group ${className}`}
-    >
-      <Image
-        src={product.imageUrl}
-        width={520}
-        height={480}
-        alt={product.name}
-        className="object-cover bg-no-repeat"
-      />
+    <Link href={`product/${product.id}`}>
+      <div
+        className={`w-[696px] h-[656px] rounded-lg bg-gradient-bg shadow-card-shadow flex items-center justify-center cursor-pointer overflow-hidden relative group ${className}`}
+      >
+        <Image
+          src={product.imageUrl}
+          width={520}
+          height={480}
+          alt={product.name}
+          className="object-cover bg-no-repeat"
+        />
 
-      <footer className="absolute bottom-1 left-1 right-1 p-8 rounded-md flex items-center justify-between bg-gray-background/60 translate-y-[110%] opacity-0 transition-all duration-300 group-hover:translate-y-0 group-hover:opacity-100">
-        <div className="flex flex-col justify-start items-start">
-          <span className="text-lg font-bold text-gray-title">
-            {product.name}
-          </span>
-          <span className="text-2xl font-bold text-main-light">
-            {product.price && formatPrice(product.price)}
-          </span>
-        </div>
-        <CartButton />
-      </footer>
-    </div>
+        <footer className="absolute bottom-1 left-1 right-1 p-8 rounded-md flex items-center justify-between bg-gray-background/60 translate-y-[110%] opacity-0 transition-all duration-300 group-hover:translate-y-0 group-hover:opacity-100">
+          <div className="flex flex-col justify-start items-start">
+            <span className="text-lg font-bold text-gray-title">
+              {product.name}
+            </span>
+            <span className="text-2xl font-bold text-main-light">
+              {product.price && formatPrice(product.price)}
+            </span>
+          </div>
+          <CartButton />
+        </footer>
+      </div>
+    </Link>
   );
 };
